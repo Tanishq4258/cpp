f6317c39a5d1a5fe3b62d16c42fe717ff389593b
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-07-08 12:43:37 +0530

    Pyrmaid

---
+++ pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,j,k;
+    for(i=1;i<=4;i++){
+        for(j=0;j<4-i;j++){
+            cout<<"  ";
+        }
+        for(k=0;k<2*i-1;k++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
