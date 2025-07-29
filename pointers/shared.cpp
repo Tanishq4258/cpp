e4ca8af7409b86ad96a133f1fe9d2897d35fd050
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-07-29 12:09:55 +0530

    fuction overload

--- pointers/shared.cpp
+++ pointers/shared.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
     shared_ptr<int> p1(new int(99));
-    shared_ptr<int> p2 = p1; // Both share the memory!
+    shared_ptr<int> p2 = p1; 
     cout << "p1: " << *p1 << ", p2: " << *p2 << endl;
     cout << "Count: " << p1.use_count() << endl;
 return 0;
