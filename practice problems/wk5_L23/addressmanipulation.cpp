cb9cac537076c07f47d3830503e38b84ab1fadcb
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-07-31 11:32:28 +0530

    added modified value

--- practice problems/wk5_L23/addressmanipulation.cpp
+++ practice problems/wk5_L23/addressmanipulation.cpp
@@ -5,6 +5,11 @@ auto sswap = [](int *x, int *y){
     *x = *y;
     *y = temp;
 };
+auto findlargest = [](int *x, int *y, int *z){
+    if (*x >= *y && *x >= *z) return x;
+    else if (*y >= *x && *y >= *z) return y;
+    else return z;
+};
 int main(){
     int a=10, b=20, c=30;
     int *p1 = &a, *p2 = &b, *p3 = &c;
@@ -15,6 +20,11 @@ int main(){
 
     cout << "After swapping: " << endl;
     cout << "a = " << a << ", b = " << b << ", c = " << c << endl;
+
+    int *largest = findlargest(p1, p2, p3);
+    cout << "Largest value is: " << *largest << endl;
+    *largest = 100;
+    cout << "Therefore Modified Value is: " << *largest << endl;
 
     return 0;
 }
